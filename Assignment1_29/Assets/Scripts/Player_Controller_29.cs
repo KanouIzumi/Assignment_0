57d32415d3a868f61aa3aaf4831e56c9fa03a78c
KanouIzumi <72852474+KanouIzumi@users.noreply.github.com>
2020-10-19 13:11:41 +0800

    Able to move the Player

--- Assignment1_29/Assets/Scripts/Player_Controller_29.cs
+++ Assignment1_29/Assets/Scripts/Player_Controller_29.cs
@@ -6,32 +6,48 @@ using UnityEngine;
 public class Player_Controller_29 : MonoBehaviour
 {
     float speed = 10.0f;
-    float maxlimit = 4.5f;
-    Rigidbody RB;
+    float zLimit = 18.30f;
+    float xLimit = 18.30f;
+    float yLimit = 0.9f;
 
+    float gravityModifier = 2.0f;
 
+    bool keypressed = false;
+
+    Rigidbody playerRb;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
+
+        Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Declare and Init variables to reference to User Interaction inouts
         float verticalInput = Input.GetAxis("Vertical");
-        float horizontalInputt = Input.GetAxis("Horizontal");
+        float horizontalInput = Input.GetAxis("Horizontal");
 
-        //Move plyer (Gameobject) according to user interactions
-        
+        transform.Translate(Vector3.forward * Time.deltaTime * verticalInput * speed);
+        transform.Translate(Vector3.right * Time.deltaTime * horizontalInput * speed);
 
-
-
-
-
-
-
+        if (transform.position.z < -zLimit)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zLimit);
+        }
+        else if (transform.position.z > zLimit)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zLimit);
+        }
+        if (transform.position.x < -xLimit)
+        {
+            transform.position = new Vector3(-xLimit, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.x > xLimit)
+        {
+            transform.position = new Vector3(xLimit, transform.position.y, transform.position.z);
+        }
     }
 }
