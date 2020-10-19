a68c3bd2cfde258e21b2ff96ff8f051b853eca32
KanouIzumi <72852474+KanouIzumi@users.noreply.github.com>
2020-10-19 12:43:21 +0800

    Trying to code the player to moving

--- Assignment1_29/Assets/Scripts/Player_Controller_29.cs
+++ Assignment1_29/Assets/Scripts/Player_Controller_29.cs
@@ -1,9 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class Player_Controller_29 : MonoBehaviour
 {
+    float speed = 10.0f;
+    float maxlimit = 4.5f;
+    Rigidbody RB;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +20,18 @@ public class Player_Controller_29 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Declare and Init variables to reference to User Interaction inouts
+        float verticalInput = Input.GetAxis("Vertical");
+        float horizontalInputt = Input.GetAxis("Horizontal");
+
+        //Move plyer (Gameobject) according to user interactions
         
+
+
+
+
+
+
+
     }
 }
