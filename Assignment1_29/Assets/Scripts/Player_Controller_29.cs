0d0715a75150fee0af9e76981ef8e750d15acf8e
KanouIzumi <72852474+KanouIzumi@users.noreply.github.com>
2020-10-21 16:31:25 +0800

    Able to do double jump and object getting destory when out of bound

--- Assignment1_29/Assets/Scripts/Player_Controller_29.cs
+++ Assignment1_29/Assets/Scripts/Player_Controller_29.cs
@@ -7,11 +7,16 @@ public class Player_Controller_29 : MonoBehaviour
     float speed = 10.0f;
     float zLimit = 18.30f;
     float xLimit = 18.30f;
+    float yLimit = 0.9f;
+
+    bool isOnFloor = false;
+    float DoubleJump = 0f;
+    float PlayersJump = 0f;
 
     float gravityModifier = 2.0f;
     private RaycastHit hit;
 
-    bool pressSpace = false;
+    
 
     Rigidbody playerRb;
 
@@ -50,7 +55,7 @@ public class Player_Controller_29 : MonoBehaviour
             transform.position = new Vector3(xLimit, transform.position.y, transform.position.z);
         }
 
-
+        /*
         if (Input.GetKeyDown(KeyCode.Space))
         {
             if (Physics.Raycast(transform.position, -Vector3.up, out hit, 1))
@@ -58,6 +63,35 @@ public class Player_Controller_29 : MonoBehaviour
                 playerRb.AddForce(Vector3.up * 10, ForceMode.Impulse);
             }
         }
+        */
 
+        //This allow me to jump when the ball is on the floor
+        if (Input.GetKeyDown(KeyCode.Space) && isOnFloor == true && DoubleJump == 0)
+        {
+            DoubleJump = 1;
+            ++PlayersJump;
+            isOnFloor = false;
+            playerRb.AddForce(Vector3.up * 10, ForceMode.Impulse);
+
+        }
+        //This allow me to jump another time on the air
+        else if (Input.GetKeyDown(KeyCode.Space) && DoubleJump == 1)
+        {
+            DoubleJump = 0;
+            ++PlayersJump;
+            playerRb.AddForce(Vector3.up * 10, ForceMode.Impulse);
+        }
+        Debug.Log("Jump Count:" + PlayersJump);
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Floor"))
+        {
+            isOnFloor = true;
+        }
+    }
+
+
 }
+
