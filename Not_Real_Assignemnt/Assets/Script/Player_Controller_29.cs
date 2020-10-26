32033f609cef0cf860bfee2bfa610d121175c7f5
KanouIzumi <shanqing649@gmail.com>
2020-10-26 11:21:59 +0800

    Another Double jump

---
+++ Not_Real_Assignemnt/Assets/Script/Player_Controller_29.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Controller_29 : MonoBehaviour
+{
+    float speed = 10.0f;
+    float zLimit = 18.30f;
+    float xLimit = 18.30f;
+    float yLimit = 0.9f;
+
+    bool isOnFloor = false;
+    float DoubleJump = 0f;
+    float SpacePress = 0f;
+
+    float gravityModifier = 2.0f;
+    private RaycastHit hit;
+
+
+
+    Rigidbody playerRb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+
+        Physics.gravity *= gravityModifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float verticalInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * verticalInput * speed);
+        transform.Translate(Vector3.right * Time.deltaTime * horizontalInput * speed);
+
+        if (transform.position.z < -zLimit)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zLimit);
+        }
+        else if (transform.position.z > zLimit)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zLimit);
+        }
+        if (transform.position.x < -xLimit)
+        {
+            transform.position = new Vector3(-xLimit, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.x > xLimit)
+        {
+            transform.position = new Vector3(xLimit, transform.position.y, transform.position.z);
+        }
+
+        /*
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (Physics.Raycast(transform.position, -Vector3.up, out hit, 1))
+            { // can jump
+                playerRb.AddForce(Vector3.up * 10, ForceMode.Impulse);
+            }
+        }*/
+
+        DoubleSpaceJump();
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Floor"))
+        {
+            isOnFloor = true;
+            SpacePress = 0;
+
+            Debug.Log("tst");
+        }
+    }
+
+    
+    private void DoubleSpaceJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if(SpacePress < 2)
+            {
+                playerRb.AddForce(Vector3.up * 10, ForceMode.Impulse);
+            }
+
+            SpacePress++;
+        }
+    }
+}
